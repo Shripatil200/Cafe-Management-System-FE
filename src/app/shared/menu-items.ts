b4945e0a9361c48b8cf6b833db5617867cf687e2
Shripatil200 <patilshriram200@gmail.com>
2026-03-04 09:43:24 +0530

    feat(category): add category and edit category

--- src/app/shared/menu-items.ts
+++ src/app/shared/menu-items.ts
@@ -1,20 +1,21 @@
 import { Injectable } from "@angular/core";
 
-export interface Menu{
+export interface Menu {
     state: string;
-    name:string;
-    type:string;
-    icon:string;
-    role:string;
+    name: string;
+    type: string;
+    icon: string;
+    role: string;
 }
 
 const MENUITEMS = [
-    {state: 'dashboard', name: 'Dashboard', type:'link', icon:'dashboard', role:''}
+    { state: 'dashboard', name: 'Dashboard', type: 'link', icon: 'dashboard', role: '' },
+    { state: 'category', name: 'Manage Category', type: 'link', icon: 'category', role: 'admin' }
 ]
 
 @Injectable()
-export class MenuItems{
-    getMenuItem():Menu[]{
+export class MenuItems {
+    getMenuItem(): Menu[] {
         return MENUITEMS;
     }
 }
