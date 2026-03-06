2982d8cf80fbe1a05d2f9223a96ea747c48c5608
Shripatil200 <patilshriram200@gmail.com>
2026-03-06 10:19:53 +0530

    feat(order): manage order page added

--- src/app/material-component/material.routing.ts
+++ src/app/material-component/material.routing.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { ManageCategoryComponent } from './manage-category/manage-category.component';
 import { RouteGuardService } from '../services/route-guard.service';
 import { ManageProductComponent } from './manage-product/manage-product.component';
+import { ManageOrderComponent } from './manage-order/manage-order.component';
 
 
 export const MaterialRoutes: Routes = [
@@ -22,6 +23,16 @@ export const MaterialRoutes: Routes = [
         canActivate:[RouteGuardService],
         data:{
             expectedRole: ['admin']
+        }
+    },
+
+    
+    {
+        path:'order',
+        component: ManageOrderComponent,
+        canActivate:[RouteGuardService],
+        data:{
+            expectedRole: ['admin','user']
         }
     }
 ];
